dbd5f00919f4fa33062152a00a339e28727420d5
Bharathisuthan <bharathisuthan@gmail.com>
2019-03-21 10:11:04 +0530

    Model and Validation updated

--- dist/models/contactModel.js
+++ dist/models/contactModel.js
@@ -1,11 +1,34 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 const mongoose = require("mongoose");
+const uniqueValidator = require("mongoose-unique-validator");
 const Schema = mongoose.Schema;
-exports.ContactSchema = new Schema({
-    id: {
-        type: Schema.ObjectId
+const AddressSchema = new Schema({
+    addLine1: {
+        type: String,
+        required: 'Address Line1 is Required.'
     },
+    addLine2: {
+        type: String
+    },
+    city: {
+        type: String,
+        required: 'City is Required.'
+    },
+    state: {
+        type: String,
+        required: 'State is Required.'
+    },
+    country: {
+        type: String,
+        required: 'Country is Required.'
+    },
+    pinCode: {
+        type: String,
+        required: 'Pin Code is Required.'
+    }
+});
+exports.ContactSchema = new Schema({
     firstName: {
         type: String,
         required: 'Enter a First Name'
@@ -15,16 +38,30 @@ exports.ContactSchema = new Schema({
         required: 'Enter a Last Name'
     },
     email: {
-        type: String
+        type: String,
+        lowercase: true,
+        unique: true,
+        required: [true, "can't be blank"],
+        match: [/\S+@\S+\.\S+/, 'is invalid'],
+        index: true
     },
     phone: {
-        type: String
+        type: String,
+    },
+    dob: {
+        type: Date
+    },
+    primaryAddress: {
+        type: AddressSchema,
+        required: 'Primary Address is Required.'
+    },
+    secondaryAddresses: {
+        type: [AddressSchema]
     },
     createdDate: {
         type: Date,
         default: Date.now
     }
-}, {
-    _id: false
 });
+exports.ContactSchema.plugin(uniqueValidator, { message: 'is already taken.' });
 //# sourceMappingURL=contactModel.js.map
